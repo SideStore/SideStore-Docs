832d9843f626a0bf61ecf56131d45473e6561969
Zahir <zahir-daiyan@tutanota.com>
2023-10-10 21:32:49 +0100

    Switch to Starlight

---
+++ astro.config.mjs
@@ -0,0 +1,47 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+export default defineConfig({
+	site: 'https://Imzxhir.github.io',
+	integrations: [
+		starlight({
+			title: 'SideStore Docs',
+			logo: {
+				alt: 'SideStore Logo',
+				src: '/src/assets/icons/sidestore-icon.png',
+			},
+			social: {
+				github: 'https://github.com/sidestore',
+				discord: 'https://discord.gg/sidestore-949183273383395328',
+				twitter: 'https://twitter.com/sidestore_io'
+			},
+			customCss: [
+				'./src/styles/custom.css',
+			],
+			sidebar: [
+				{
+					label: 'FAQ',
+					link: 'faq',
+				},
+				{
+					label: 'Guides',
+					items: [
+						{ label: 'Getting Started', link: '/guides/getting-started/' },
+						{ label: 'Create a Burner Apple ID', link: '/guides/burner-apple-id/' },
+						{ label: 'Troubleshooting', link: '/guides/troubleshooting/' },
+						{ label: 'Error Codes', link: '/guides/error-codes/' },
+					],
+				},
+				{
+					label: 'Advanced',
+					items: [
+						{ label: 'Custom Anisette Server', link: '/advanced/anisette-server/' },
+						{ label: 'App Sources', link: '/advanced/app-sources/' },
+						{ label: 'URL Schemes', link: '/advanced/url-schemes/' },
+					]
+
+				},
+			],
+		}),
+	],
+});
