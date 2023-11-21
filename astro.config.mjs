2faf49e4eacbfc4e6c4b78de42d10b847c7934f0
Imzxhir <98621617+Imzxhir@users.noreply.github.com>
2023-11-21 20:48:09 +0000

    Fix an error with formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  trailingSlash: 'always',
   site: 'https://wiki.sidestore.io',
   integrations: [
     starlight({
