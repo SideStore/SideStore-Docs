043d1aad86215eeaad58896f0afbee4dd1e4f78e
Charlie <147570875+I-794@users.noreply.github.com>
2024-12-14 23:05:03 -0600

    Update docusaurus.config.js
    Added bluesky to community on the bottom
    
    Signed-off-by: Charlie <147570875+I-794@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -111,6 +111,9 @@ const config = {
                 label: 'Patreon',
                 href: 'https://www.patreon.com/sidestore',
               },
+              {
+                label: 'Bluesky',
+                href: 'https://bsky.app/profile/sidestore.io',
             ],
           },
           {
