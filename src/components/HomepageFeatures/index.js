d6db6d81b43e87374d709214924b5554c3b4bc83
basketshoe <54729294+basketshoe@users.noreply.github.com>
2025-12-18 15:19:26 +0100

    Add translation to landing page
    Via `write-translations`.
    
    Note:
    Couple of warnings are printed since it does not like the fact that in src/pages/index.js lines 17 and 20 contain dynamic values (specifically `siteConfig.title` and `siteConfig.tagline`).
    
    Run `yarn write-translations --locale en` to see the full log

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,33 +1,34 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import Translate, {translate} from '@docusaurus/Translate';
 
 const FeatureList = [
   {
-    title: 'Fast, Easy, Sleek',
+    title: translate({id: 'home.feature.FES.title', message: 'Fast, Easy, Sleek'}),
     description: (
-      <>
+      <Translate id="home.feature.FES.description">
         SideStore is meant to be a functional application, and in being functional
         is good looking, and not overcomplicated.
-      </>
+      </Translate>
     ),
   },
   {
-    title: 'Power is Key',
+    title: translate({id: 'home.feature.power.title', message: 'Power is Key'}),
     description: (
-      <>
+      <Translate id="home.feature.power.description">
         The ability to install applications not found on Apple's App Store is not
         common, and SideStore fills a large gap in the sideloading field.
-      </>
+      </Translate>
     ),
   },
   {
-    title: 'Open Source',
+    title: translate({id: 'home.feature.openSource.title', message: 'Open Source'}),
     description: (
-      <>
+      <Translate id="home.feature.openSource.description">
         We find open source software very important, as SideStore is itself open source.
         Anyone is welcome to make contributions to any SideStore project on GitHub.
-      </>
+      </Translate>
     ),
   },
 ];
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Translate, {translate} from '@docusaurus/Translate';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
@@ -13,14 +14,16 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          {translate({id: 'home.heroTitle', message: siteConfig.title})}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">
+           <Translate id="home.heroSubtitle">{siteConfig.tagline}</Translate>
+        </p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Get Started!
+              <Translate id="home.getStartedButton">Get Started</Translate>
           </Link>
         </div>
       </div>
